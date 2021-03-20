dd41eba8e23a637ee95ccf88ca2b2464ec853776
Yarik <yaroslav.matsapura@gmail.com>
2021-03-21 00:34:28 +0200

    Linked list for Pairs

--- Translator/Translator/Source.cpp
+++ Translator/Translator/Source.cpp
@@ -1,73 +1,84 @@
+
 #include <iostream>
 #include <string>
 #include <fstream>
 using namespace std;
 class Pair {
-	public:
-		string Key;
-		string Value;
-
+public:
+	string Key;
+	string Value;
+	void set_Key_Value(string a, string b) {
+		Key = a;
+		Value = b;
+	}
 };
 //Linked list
 class Node {
-	public:
-		string data;
-		Node* next;
-		Node(string x) {
-			data = x;
-			next = NULL;
-		}
+public:
+	Pair data;
+	Node* next;
+	Node(Pair x) {
+		data = x;
+		next = NULL;
+	}
 };
 class LinkedList {
-	
-	public:
-		Node* head;
-		bool isEmpty() {
-			if (head == NULL) {
-				return 1;
-			}
-			else {
-				return 0;
-			}
+
+public:
+	Node* head;
+	bool isEmpty() {
+		if (head == NULL) {
+			return 1;
 		}
-		void addAtEnd(string word) {
-			Node* n = new Node(word);
-			if (head == NULL) {
-				head = n;
-				n->next = NULL;
-			}
-			else {
-				Node* n2 = getLastNode();
-				n2->next = n;
-			}
+		else {
+			return 0;
 		}
-		Node* getLastNode() {
-			Node* ptr = head;
-			while (ptr->next != NULL) {
-				ptr = ptr->next;
-			}
-			return ptr;
+	}
+	void addAtEnd(Pair pair) {
+		Node* n = new Node(pair);
+		if (head == NULL) {
+			head = n;
+			n->next = NULL;
 		}
-		void display() {
-			Node* pointer = head;
-			while (pointer != NULL) {
-				cout << pointer->data << "->";
-				pointer=pointer->next;
-			}
+		else {
+			Node* n2 = getLastNode();
+			n2->next = n;
 		}
+	}
+	Node* getLastNode() {
+		Node* ptr = head;
+		while (ptr->next != NULL) {
+			ptr = ptr->next;
+		}
+		return ptr;
+	}
+	/*void display() {
+		Node* pointer = head;
+		while (pointer != NULL) {
+			
+			cout << pointer->data << "->";
+			pointer = pointer->next;
+		}
+	}*/
 
 
-		LinkedList() {
-			head = NULL;
-		}
+	LinkedList() {
+		head = NULL;
+	}
 };
 
 
 
 int main() {
 	LinkedList list;
-	list.addAtEnd("aa");
-	list.addAtEnd("bb");
-	list.addAtEnd("cc");
-	list.display();
+	Pair a;
+	a.set_Key_Value("one", "word1");
+	Pair b;
+	a.set_Key_Value("two", "word2");
+	Pair c;
+	a.set_Key_Value("three", "word3");
+	list.addAtEnd(a);
+	list.addAtEnd(b);
+	list.addAtEnd(c);
+	
 }
